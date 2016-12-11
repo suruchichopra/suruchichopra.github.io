c234296e42978a13ba6107efd877ba81b3c50cab
suri97 <suransh2008@gmail.com>
2016-12-11 11:42:58 +0530

    Fixed bug in contact

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,16 @@
 /**
  * Created by SURANSH on 12/8/2016.
  */
+$(function () {
 
+    if ( $(window).width() <= 520) {
+        $('#photo').removeClass();
+        $('#photo').addClass('pull-center');
+    }
+    else {
+        $('#photo').removeClass();
+        $('#photo').addClass('pull-right');
+    }
 $(window).on('resize', function(){
     var win = $(this);
     if (win.width() <= 520) {
@@ -13,3 +22,4 @@ $(window).on('resize', function(){
         $('#photo').addClass('pull-right');
     }
 });
+})
