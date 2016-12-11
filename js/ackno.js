8027cada007712b0b771950617c9286d3d8438fd
suri97 <suransh2008@gmail.com>
2016-12-11 11:16:20 +0530

    Fixed bug in ackno

--- js/ackno.js
+++ js/ackno.js
@@ -2,6 +2,26 @@
  * Created by SURANSH on 12/8/2016.
  */
 
+if ( $(window).width() <= 600) {
+    $('#momdad').removeClass();
+    $('#momdad').addClass('pull-center');
+    $('#mam').removeClass();
+    $('#mam').addClass('pull-center');
+    $('#shivali').removeClass();
+    $('#shivali').addClass('pull-center');
+    $('#suransh').removeClass();
+    $('#suransh').addClass('pull-center');
+}
+else {
+    $('#momdad').removeClass();
+    $('#momdad').addClass('pull-left');
+    $('#mam').removeClass();
+    $('#mam').addClass('pull-right');
+    $('#shivali').removeClass();
+    $('#shivali').addClass('pull-left');
+    $('#suransh').removeClass();
+    $('#suransh').addClass('pull-right');
+}
 $(window).on('resize', function(){
     var win = $(this);
     if (win.width() <= 600) {
